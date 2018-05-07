29ef2d24fd20ac071b53aa638305cb7c56269a6b
Chris Gioeli <cgioeli@gmail.com>
2018-05-07 19:47:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {Savatage: "Edge"};
 function updatePlaylist(playlist, Savatage, Edge){
-  return Object.assign({}, playlist, {[Slayer]: "Angel"})
+  return Object.assign({}, playlist, {["Slayer"]: "Angel"})
 }
