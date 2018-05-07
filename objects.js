d22974f6a314dc09b1c0fd57cbabf9ed5b92067e
Chris Gioeli <cgioeli@gmail.com>
2018-05-07 20:14:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName: "songTitle"};
+var playlist = {savatage: "songTitle"};
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist.assign({}, playlist, { [savatage]: "sleep" })
 }
