fa381b11023076e676bc10a25165cfd6ce154cf7
Chris Gioeli <cgioeli@gmail.com>
2018-05-07 21:07:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 function removeFromPlaylist (playlist, artistName){
-  delete artistName.artistName
+  delete artistName.Slowdive
   return playlist
 }
